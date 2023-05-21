e95ea92dfdeb436644d074c6cd730c4b9787bc5c
vipul chakravarthy <vipulchakravarthy.r@gmail.com>
2023-05-21 17:05:50 +0530

    removed error block

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,6 @@
 import Link from "next/link";
 
 export default function About() {
-  throw new Error("Testing error handling");
   return (
     <div className="text-center mt-10">
       <p>Oh bro! on about page</p>
